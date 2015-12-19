d3b95e350a94e2234af8e63ce8d943a824f96350
paris <paris@paris-P5QC>
2015-12-19 12:14:30 +0200

    Add routing.php and services.php. Fix typo on index.php

---
+++ config/services.php
@@ -0,0 +1,83 @@
+<?php 
+
+use \Phalcon\Logger\Adapter\File as Logger;
+
+//The session service is available by default, but we overwrite it
+//because we want to use Redis to store our session
+$di['session'] = function() use ($config) {
+	$session = new \Phalcon\Session\Adapter\Redis(array(
+		'uniqueId' => $config->session->unique_id,
+		'path' 		 => $config->session->path,
+		'name' 		 => $config->session->name,
+	));
+
+	$session->start();
+
+	return $session;
+};
+
+//The security session is available by default, but we overwrite it
+//because we want to set it's options
+$di['security'] = function() {
+	$security = new \Phalcon\Security();
+	$security->setWorkFactor(10);
+
+	return $security;
+};
+
+
+//Connect to the Redis server
+$di['redis'] = function() use ($config) {
+	$redis = new \Redis();
+	$redis->connect($config->redis->host, $config->redis->port);
+ 
+	return $redis;
+};
+
+//Overwrite for backwards compatibility
+$di['url'] = function() use ($config) {
+	$url = new \Phalcon\Mvc\Url();
+
+	return $url;
+};
+
+//Custom DI component to use for the Volt template engine
+$di['voltService'] = function($view, $di) use ($config) {
+	$volt = new \Phalcon\Mvc\View\Engine\Volt($view, $di);
+
+	if(!is_dir($config->view->cache->dir)){
+		mkdir($config->view->cache->dir);
+	}
+
+	$volt->setOptions(array(
+		'compiledPath' => $config->view->cache->dir,
+		'compiledExtension' => '.compiled',
+		'compileAlways' => true
+	));
+
+	return $volt;
+};
+
+//Log errors in the logs folder
+$di['logger'] = function() {
+	$file = __DIR__.'/../logs/'.date('Y-m-d').'.log';
+	$logger = new Logger($file, array(
+		'mode' => 'w+'
+	));
+
+	return $logger;
+};
+
+$di['cache'] = function() use ($di, $config) {
+	$frontend = new \Phalcon\Cache\Frontend\Igbinary(array(
+		'lifetime' => 3600*24
+	));
+
+	$cache = new \Phalcon\Cache\Backend\Redis($frontend, array(
+		'redis' => $di['redis'],
+		'prefix' => $config->application->name.':'
+	));
+
+	return $cache;
+};
+
--- public/index.php
+++ public/index.php
@@ -1,9 +1,13 @@
 <?php 
 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+
 header('Content-Type: text/html; charset=utf-8');
 mb_internal_encoding('UTF-8');
 
-require_once __DIR__.'../modules/Bootstrap.php';
+require_once __DIR__.'/../modules/Bootstrap.php';
 
 $app = new Bootstrap('frontend');
 $app->init();
