45b1b5573c43799d33818bbe37fa4ac5a7c0cf9b
Paris <parispar@users.noreply.github.com>
2015-12-20 10:13:50 +0200

    Fix config.php

--- modules/Frontend/Config/config.php
+++ modules/Frontend/Config/config.php
@@ -4,16 +4,16 @@
 $config = require __DIR__.'/../../../config/config.php';
 
 //Assign module specific configuration
-$module_config = array(
+$module_config = new \Phalcon\Config(array(
 	'application' => array(
-		'controllersDir' => __DIR__.'/../Controllers',
-		'controllersDir' => __DIR__.'/../Controllers',
-		'controllersDir' => __DIR__.'/../Controllers',
+		'controllersDir' => __DIR__.'/../Controllers/',
+		'modelsDir' => __DIR__.'/../Models/',
+		'viewsDir' => __DIR__.'/../Views/',
 		'baseUri' => '/',
 		'cryptSalt' => 'aoj90g89d5jgd95g8',
 		'publicUrl' => 'http://test.com/learning-phalcon/'
 	)
-);
+));
 
 $config->merge($module_config);
 
