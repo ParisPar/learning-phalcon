d5485a6448f4c75c6b98fc7dfb2e846fba572e55
paris <parispar@users.noreply.github.com>
2015-12-24 07:39:44 +0200

    Add limitation to number of request from the same IP

--- modules/Core/Listeners/ApiListener.php
+++ modules/Core/Listeners/ApiListener.php
@@ -8,13 +8,14 @@ class ApiListener extends \Phalcon\Mvc\User\Plugin {
 	//dispatcher has initialized the controller and knows if the action exists.
 	public function beforeExecuteRoute($event, $dispatcher) {
 		$hasValidKey = $this->checkForValidApiKey();
+		$ipRateLimit = $this->checkIpRateLimit();
 
-		if($hasValidKey == false) {
+		if($hasValidKey == false || $ipRateLimit == false) {
 			return false;
 		}
 	}
 
-	public function checkForValidApiKey() {
+	private function checkForValidApiKey() {
 		$apiKey = $this->request->getHeader('APIKEY');
 
 		if(!in_array($apiKey, $this->config->apiKeys->toArray())) {
@@ -24,6 +25,36 @@ class ApiListener extends \Phalcon\Mvc\User\Plugin {
 			$this->view->disable();
 
 			return false;
+		}
+
+		return true;
+	}
+
+	private function checkIpRateLimit() {
+		$ip = $this->request->getClientAddress();
+		$time = time();
+		$key = $ip.':'.$time;
+
+		$redis = $this->getDI()->get('redis');
+		$current = $redis->get($key);//Get the value related to the specified key
+
+		if($current != null && $current > 5) {
+			$this->response->setStatusCode(429, 'Too Many Requests');
+			$this->response->sendHeaders();
+			$this->response->send();
+			$this->view->disable();
+
+			return false;
+		} else {
+			//returns the Redis instance and enters multi-mode. Once in multi-mode, 
+			//all subsequent method calls return the same object until exec() is called.
+			$redis->multi();
+
+			$redis->incr($key, 1);//Increment the number stored at key by one.
+			$redis->expire($key, 5);//Sets an expiration date (a timeout) on an item. pexpire requires a TTL in milliseconds.
+
+			$redis->exec();
+
 		}
 
 		return true;
